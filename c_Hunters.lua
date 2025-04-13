72421159fe0bfcc328768fd525f4fa55bb1ccbfa
SangiDn277 <lucassangino217@gmail.com>
2025-04-13 16:48:48 -0300

    Se proceso Correctamente la orientacion

--- c_Hunters.lua
+++ c_Hunters.lua
@@ -1,12 +1,26 @@
 
+local Hunter = createPed(33,-1628.8681640625, -2239.294921875,31.4765625)
+
+    setPedRotation(Hunter,90)
+    setElementHealth(Hunter, 100)
+    setPedArmor(Hunter, 100)
+    setElementFrozen(Hunter, true) -- Evita que el NPC se mueva solo
+    setElementData(Hunter, "npc", true) -- Identificar como NPC
+
+    
+
+
 marcador = createMarker ( -1630.025390625, -2239.4326171875, 30 ,"cylinder", 1.0,255, 162, 51, 255 )
 
 function animacion(hitPlayer)
   
       if hitPlayer == localPlayer then
          
-        triggerServerEvent("animarHunter",localPlayer) -- Pide al servidor que anime al ped
-      
+         setPedAnimation(Hunter, "PED", "IDLE_chat")
+         setTimer(setPedAnimation, 5000, 1, Hunter)
+
+        
+
     triggerEvent("panelWindowHunt", resourceRoot)
     end
   
--- s_huntingManager.lua
+++ s_huntingManager.lua
@@ -1,19 +1,4 @@
 
- local hunter = createPed( 33, -1628.8681640625, -2239.294921875,31.4765625 , 85)
-setPedRotation(hunter, 85)
-
- if not hunter then
-        outputChatBox("Error al crear el NPC")
-        return
-    end
-     
-    setElementHealth(hunter, 100)
-    setPedArmor(hunter, 100)
-    setElementFrozen(hunter, true) -- Evita que el NPC se mueva solo
-    setElementData(hunter, "npc", true) -- Identificar como NPC
-
-    
-
 
 function animarHunter()
     setPedAnimation(hunter, "PED", "IDLE_chat")
